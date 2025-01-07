a270b8d446705ddafaa7c91cbabed8d5cbdee5a2
Raj Shah <rajnshah2020@gmail.com>
2025-01-07 00:56:34 -0500

    change name

--- src/lib/translation/i18n.ts
+++ src/lib/translation/i18n.ts
@@ -1,6 +1,6 @@
 export const locale = {
 	en: {
-		title: 'PATHVISOR',
+		title: 'Welcome to PATHVISOR',
 		pickAlgorithm: 'Pick an algorithm to start.',
 		selectStartOrEnd: 'Click the Start (🟦) or End (🟧) node once to change its position.',
 		usePlayer:
